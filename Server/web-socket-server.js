0cbe631c60619858b51fa0afea16ea6aaf371e3b
Tom Scallon <tssnowguy13@gmail.com>
2016-05-31 20:50:00 -0500

    Shell implementation of the Web Socket server

--- Server/web-socket-server.js
+++ Server/web-socket-server.js
@@ -1,5 +1,23 @@
-const WebSocketServer = require("ws").Server;
+const WebSocket = require("ws");
 const httpServer = require("./http-server");
-const server = new WebSocketServer({ server: httpServer });
+const server = new WebSocket.Server({ server: httpServer });
+
+const onConnection = (ws) => {
+  console.log("Received new connection: ", ws);
+};
+
+const onMessage = (ws, message) => {
+  
+};
+
+WebSocket.prototype.sendJson = (data) => {
+  this.send( JSON.stringify(data) );
+};
+
+WebSocket.Server.prototype.broadcast = (data) => {
+  this.clients.forEach(client => {
+    client.sendJson( data );
+  });
+};
 
 module.exports = server;
