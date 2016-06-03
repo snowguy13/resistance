2073c867a569eef78c4aedde6dbf1b0b0c3db334
Tom Scallon <tssnowguy13@gmail.com>
2016-06-02 19:55:17 -0500

    Remove app.bundle.js(.map) from tracking, add redux and react-redux depencies, rename 'server' script to 'start', create basic entry to app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Resistance from './components/resistance';
 
+render( <Resistance />, document.getElementById('root') );
---
+++ src/components/resistance.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Resistance extends Component {
+  render() {
+    return (<div className="resistance">Loaded!</div>);
+  }
+};
+
+export default Resistance;
