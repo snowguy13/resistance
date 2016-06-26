385e651461a5e8fe73fc60b7e153d3a5dbb63602
Tom Scallon <tssnowguy13@gmail.com>
2016-06-26 11:54:29 -0500

    Add login buttons and styling

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -7,7 +7,28 @@ class Login extends Component {
   render() {
     return (
       <div className="Login">
-        Login stuff here!
+        <div className="Login__Message">
+          Please identify yourself.
+        </div>
+        <div className="Login__Input">
+          <label for="Login__Username">
+            Agent ID
+          </label>
+          <input type="text" placeholder="Agent ID" />
+        </div>
+        <div className="Login__Input">
+          <label for="Login__Password">
+            Auth Code
+          </label>
+          <input type="password" placeholder="Auth Code" />
+        </div>
+        <div className="Login__Buttons">
+          <button className="Login__Button">Authenticate</button>
+        </div>
+        <div className="Login__Buttons">
+          <button className="Login__Button">Register</button>
+          <button className="Login__Button">Temp Agent</button>
+        </div>
       </div>
     );
   }
