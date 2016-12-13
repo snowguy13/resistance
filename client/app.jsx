0759170b4b61f106f11d57ce86b76b44cba133a3
Tom Scallon <tssnowguy13@gmail.com>
2016-12-13 15:53:22 -0600

    Add redux-thunk as middlware

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import Resistance from './components/resistance';
 import reducers from './reducers/all';
 
@@ -13,7 +14,14 @@ if( !window.WebSocket ) {
   root.innerHTML = "Looks like your browser doesn't support WebSockets. "
    + "Unfortunately, they're needed to run this game, so you're out of luck!";
 } else {
-  let store = createStore( reducers, {}, window.devToolsExtension && window.devToolsExtension() );
+  let store = createStore(
+    reducers,
+    {},
+    compose(
+      window.devToolsExtension ? window.devToolsExtension() : () => (),
+      applyMiddleware( reduxThunk )
+  );
+
   render(
     <Provider store={store}>
       <Resistance socketUrl={`ws://${window.location.host}`} />
